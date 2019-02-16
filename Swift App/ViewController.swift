c115e95a500f333ae9f2a5301080ad56aee8e2b8
Kaitlyn  Caracciolo <katiecaracciolo@kaitlyns-imac.local>
2019-02-16 09:54:13 -0600

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Kaitlyn  Caracciolo on 2/16/19.
+//  Copyright © 2019 George Andersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

52787a36c227b7805d40a8309c2a0bc33f845162
george andersen <cgeorgeandersen@GMAIL.COM>
2019-02-16 21:18:49 -0600

    finished? app

---
+++ Emoji Dictionary/Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Kaitlyn  Caracciolo on 2/16/19.
+//  Copyright © 2019 George Andersen. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    var emoji = "NO EMOJI"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         print(emoji)
+        
+        
+        
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+    
+
+    
+    
+    
+
+
+}
--- Emoji Dictionary/Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/Emoji Dictionary/ViewController.swift
@@ -32,6 +32,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+      performSegue(withIdentifier: "MoveSegue", sender: emoji)    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let defVC = segue.destination as! DefinitionViewController
+        defVC.emoji = sender as! String
+        
+        
+        
+    }
 }
 
 
